79d0e88bd7ae0520a99180b3c2b66d2f5f59459f
BarLouvain <barlouvain@gmail.com>
2026-05-04 13:33:09 +0200

    menukaart mei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         />
 
         <a
-          href="/menu2026.pdf" 
+          href="/menukaart.pdf" 
           target="_blank"
           rel="noopener noreferrer"
           style={{
